9c301281d551343aa155f157ece540b1ac432016
sharjeel sattar <sharjeel.sattar@infotechgroup.com>
2022-09-09 13:31:41 +0500

    delete all for revamp

--- dms-saas/dms-saas/userService/src/main/java/com/infotech/user/service/repository/UserRepository.java
+++
@@ -1,7 +0,0 @@
-package com.infotech.user.service.repository;
-
-import com.infotech.user.service.entity.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepository extends JpaRepository<User,Long> {
-}
