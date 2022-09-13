74de54bd5dfa0e1e3df53c3925d295467ad687e7
Haris.Ahmed <haris.ahmed@infotechgroup.com>
2022-09-13 15:06:46 +0500

    core issue resolved

--- UserManagement/src/main/java/com/infotech/docyard/UMServicesApplication.java
+++ UserManagement/src/main/java/com/infotech/docyard/UMServicesApplication.java
@@ -3,7 +3,13 @@ package com.infotech.docyard;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.core.Ordered;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
 
 @SpringBootApplication
 public class UMServicesApplication extends SpringBootServletInitializer {
@@ -18,4 +24,17 @@ public class UMServicesApplication extends SpringBootServletInitializer {
         return application.sources(UMServicesApplication.class);
     }
 
+    @Bean
+    public FilterRegistrationBean corsFilter() {
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        CorsConfiguration config = new CorsConfiguration();
+        config.setAllowCredentials(true);
+        config.addAllowedOriginPattern(CorsConfiguration.ALL);
+        config.addAllowedHeader("*");
+        config.addAllowedMethod("*");
+        source.registerCorsConfiguration("/**", config);
+        FilterRegistrationBean bean = new FilterRegistrationBean(new CorsFilter(source));
+        bean.setOrder(Ordered.HIGHEST_PRECEDENCE);
+        return bean;
+    }
 }
