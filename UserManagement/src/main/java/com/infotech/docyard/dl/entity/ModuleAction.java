05c398b7aac64c32f4ea86db39726af417c02d04
Haris.Ahmed <haris.ahmed@infotechgroup.com>
2022-09-14 17:40:29 +0500

    modules entity, api done

---
+++ UserManagement/src/main/java/com/infotech/docyard/dl/entity/ModuleAction.java
@@ -0,0 +1,37 @@
+package com.infotech.docyard.dl.entity;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+
+import javax.persistence.*;
+
+@Data
+@Entity
+@Table(name = "MODULES_ACTIONS")
+@JsonIgnoreProperties(ignoreUnknown = true)
+@EqualsAndHashCode(callSuper = false)
+public class ModuleAction extends BaseEntity {
+
+    @Column(name = "TITLE", unique = true, nullable = false)
+    private String title;
+
+    @Column(name = "SLUG", unique = true, nullable = false)
+    private String slug;
+
+    @Column(name = "SEQ", nullable = false)
+    private Integer seq;
+
+    @JsonIgnore
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "MODULE_ID", nullable = false)
+    private Module module;
+
+    public ModuleAction() {
+    }
+
+    public ModuleAction(Long id) {
+        this.setId(id);
+    }
+}
