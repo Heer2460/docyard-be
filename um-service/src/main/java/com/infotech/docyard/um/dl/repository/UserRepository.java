4b176cc5aa9ce8f7a96961bedbc9bf074d0c830b
sharjeel sattar <sharjeel.sattar@infotechgroup.com>
2022-10-12 14:58:28 +0500

    multiple fixes

--- um-service/src/main/java/com/infotech/docyard/um/dl/repository/UserRepository.java
+++ um-service/src/main/java/com/infotech/docyard/um/dl/repository/UserRepository.java
@@ -14,6 +14,6 @@ public interface UserRepository extends JpaRepository<User, Long> {
 
     Optional<User> findByEmail(String email);
 
-    boolean existsByGroup_Id(Long groupId);
+    boolean existsByGroup_IdAndStatus(Long groupId,String status);
 
 }
--- um-service/src/main/java/com/infotech/docyard/um/service/GroupService.java
+++ um-service/src/main/java/com/infotech/docyard/um/service/GroupService.java
@@ -3,10 +3,12 @@ package com.infotech.docyard.um.service;
 import com.infotech.docyard.um.dl.entity.Group;
 import com.infotech.docyard.um.dl.repository.AdvSearchRepository;
 import com.infotech.docyard.um.dl.repository.GroupRepository;
+import com.infotech.docyard.um.dl.repository.GroupRoleRepository;
 import com.infotech.docyard.um.dl.repository.UserRepository;
 import com.infotech.docyard.um.dto.GroupDTO;
 import com.infotech.docyard.um.exceptions.DBConstraintViolationException;
 import com.infotech.docyard.um.exceptions.DataValidationException;
+import com.infotech.docyard.um.util.AppConstants;
 import com.infotech.docyard.um.util.AppUtility;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -28,12 +30,13 @@ public class GroupService {
     private AdvSearchRepository advSearchRepository;
     @Autowired
     private UserRepository userRepository;
+    @Autowired
+    private GroupRoleRepository groupRoleRepository;
 
-
-    public List<Group> searchGroup(String code, String name, String status, List<Long>role) {
+    public List<Group> searchGroup(String code, String name, String status, List<Long> role) {
         log.info("searchGroup method called..");
 
-        return advSearchRepository.searchGroup(code, name, status,role);
+        return advSearchRepository.searchGroup(code, name, status, role);
     }
 
     public List<Group> getAllGroup() {
@@ -57,10 +60,10 @@ public class GroupService {
         log.info("saveAndUpdateGroup method called..");
 
         Group group = groupDTO.convertToEntity();
-        if(AppUtility.isEmpty(group.getGroupRoles())){
+        if (AppUtility.isEmpty(group.getGroupRoles())) {
             group.setGroupRoles(groupDTO.groupRoles(group));
         }
-        if(groupRepository.existsByCode(groupDTO.getCode())){
+        if (groupRepository.existsByCode(groupDTO.getCode())) {
             throw new DBConstraintViolationException("Code Already Exists");
         }
         Group groups = groupRepository.save(group);
@@ -72,18 +75,24 @@ public class GroupService {
         log.info("saveAndUpdateGroup method called..");
 
         Group group = groupDTO.convertToEntity();
-        if(AppUtility.isEmpty(group.getGroupRoles())){
+
+        if (groupDTO.getStatus().equalsIgnoreCase(AppConstants.Status.SUSPEND)) {
+            if (groupRoleRepository.existsByGroup_IdAndRole_Status(groupDTO.getId(), AppConstants.Status.ACTIVE)
+                    || userRepository.existsByGroup_IdAndStatus(groupDTO.getId(), AppConstants.Status.ACTIVE)) {
+                throw new DataValidationException(AppUtility.getResourceMessage("record.cannot.be.deleted.dependency"));
+            }
+        }
+        if (AppUtility.isEmpty(group.getGroupRoles())) {
             group.setGroupRoles(groupDTO.groupRoles(group));
         }
-        Group groups = groupRepository.save(group);
-        return groups;
+        return groupRepository.save(group);
     }
 
     @Transactional
     public void deleteGroup(Long id) {
         log.info("deleteGroup method called..");
 
-        if(userRepository.existsByGroup_Id(id)){
+        if (userRepository.existsByGroup_IdAndStatus(id, AppConstants.Status.ACTIVE)) {
             throw new DataValidationException(AppUtility.getResourceMessage("record.cannot.be.deleted.dependency"));
         }
 
