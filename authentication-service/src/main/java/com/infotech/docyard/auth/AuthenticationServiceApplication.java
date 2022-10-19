957aa52f79bb8fc90a1b7eeaa4848db57aec003e
Irfan Nasim <irfan.nasim@infotechgroup.com>
2022-10-19 10:52:13 +0500

    refacorting

--- authentication-service/src/main/java/com/infotech/docyard/auth/AuthenticationServiceApplication.java
+++ authentication-service/src/main/java/com/infotech/docyard/auth/AuthenticationServiceApplication.java
@@ -11,6 +11,7 @@ public class AuthenticationServiceApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(AuthenticationServiceApplication.class, args);
+
         System.out.println("AuthenticationServiceApplication server running.......");
     }
 }
