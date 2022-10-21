efa5ab7fe51490130e1e1a9bda1642983a62d167
sharjeel sattar <sharjeel.sattar@infotechgroup.com>
2022-10-21 10:58:50 +0500

    local commit

---
+++ job-service/src/main/java/com/infotech/docyard/js/JobServicesApplication.java
@@ -0,0 +1,21 @@
+package com.infotech.docyard.js;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.context.annotation.Bean;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.web.client.RestTemplate;
+
+@SpringBootApplication
+@EnableEurekaClient
+@EnableScheduling
+public class JobServicesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JobServicesApplication.class, args);
+        System.out.println("JobServicesApplication server running.......");
+    }
+
+}
---
+++ job-service/src/main/java/com/infotech/docyard/js/config/SFTPProperties.java
@@ -0,0 +1,24 @@
+package com.infotech.docyard.js.config;
+
+import lombok.Data;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@Data
+@ConfigurationProperties(ignoreUnknownFields = false, prefix = "sftp.client")
+public class SFTPProperties {
+
+    private String host;
+    private Integer port;
+    private String protocol;
+    private String username;
+    private String password;
+    private String root;
+    private String privateKey;
+    private String passphrase;
+    private String sessionStrictHostKeyChecking;
+    private Integer sessionConnectTimeout;
+    private Integer channelConnectedTimeout;
+
+}
