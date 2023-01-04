9cbe81876b5df58a2ac3ff50cffe2abbf06e1ea7
Tahir\admin <tidress@indra.es>
2023-01-04 15:42:50 +0500

    Permissions table removed from db and all existing code also removed related to permissions.

--- um-service/src/main/java/com/infotech/docyard/um/dl/repository/PermissionRepository.java
+++
@@ -1,7 +0,0 @@
-package com.infotech.docyard.um.dl.repository;
-
-import com.infotech.docyard.um.dl.entity.Permission;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface PermissionRepository extends JpaRepository<Permission, Long> {
-}
